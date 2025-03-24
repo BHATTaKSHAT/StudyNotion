c57e15afc4194a99fe823d1031ecd87a3c5109df
Akshat Bhatt <bakshat265@gmail.com>
2025-03-24 22:40:45 +0530

    updated css

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -27,19 +27,23 @@ const Homepage = () => {
 
       <div className="hero-section">
         <h1 className="animated-text">Welcome to Study Notion</h1>
-        <p className="hero-subtitle">Your Gateway to Interactive Learning</p>
+        <p className="hero-subtitle">
+          YOUR <span className="highlight">GATEWAY</span> <br></br>
+          TO<br></br>
+          INTERACTIVE <span className="blue">LEARNING</span>
+        </p>
         <div className="features">
-          <div className="feature-card">
+          <div className="feature-card  icon-book">
             <span className="feature-icon">📚</span>
             <h3>Rich Content</h3>
             <p>Access comprehensive courses with video lectures and articles</p>
           </div>
-          <div className="feature-card">
+          <div className="feature-card  icon-quiz">
             <span className="feature-icon">✍️</span>
             <h3>Interactive Quizzes</h3>
             <p>Test your knowledge with engaging quizzes</p>
           </div>
-          <div className="feature-card">
+          <div className="feature-card icon-progress">
             <span className="feature-icon">📊</span>
             <h3>Track Progress</h3>
             <p>Monitor your learning journey with detailed progress tracking</p>
