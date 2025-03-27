559a17eb43a05740a1151056f818a231d2387898
Akshat Bhatt <bakshat265@gmail.com>
2025-03-27 18:27:25 +0530

    updated homepage

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./Homepage.css";
+import pencilImage from "../assets/pencil.png";
+import book from "../assets/book.png";
+import prplball from "../assets/prplball.png";
+import earth from "../assets/earth.png";
+import tag from "../assets/tag.png";
 
 const Homepage = () => {
   const navigate = useNavigate();
@@ -27,27 +32,38 @@ const Homepage = () => {
 
       <div className="hero-section">
         <h1 className="animated-text">Welcome to Study Notion</h1>
+        <img src={pencilImage} alt="pencil" className="pencil-image" />
+        <img src={book} alt="book" className="book-image" />
+        <img src={prplball} alt="prplball" className="prplball-image" />
+        <img src={earth} alt="earth" className="earth-image" />
+        <img src={tag} alt="tag" className="tag-image" />
         <p className="hero-subtitle">
-          YOUR <span className="highlight">GATEWAY</span> <br></br>
-          TO<br></br>
-          INTERACTIVE <span className="blue">LEARNING</span>
+          CREATE NEW<br></br>
+          <span className="highlight">EXPERIENCE</span> WITH <br></br>
+          <span className="outline">WAYS OF</span>
+          <br></br>
+          PERFECT <span className="blue">LEARNING</span>
         </p>
-        <div className="features">
-          <div className="feature-card  icon-book">
-            <span className="feature-icon">📚</span>
-            <h3>Rich Content</h3>
-            <p>Access comprehensive courses with video lectures and articles</p>
-          </div>
-          <div className="feature-card  icon-quiz">
-            <span className="feature-icon">✍️</span>
-            <h3>Interactive Quizzes</h3>
-            <p>Test your knowledge with engaging quizzes</p>
-          </div>
-          <div className="feature-card icon-progress">
-            <span className="feature-icon">📊</span>
-            <h3>Track Progress</h3>
-            <p>Monitor your learning journey with detailed progress tracking</p>
-          </div>
+      </div>
+      <div className="features">
+        {/* <h2 className="features-subtitle">
+            ENHANCE YOUR <br></br>
+            WITH ENGAGING LEARNING TOOLS
+          </h2> */}
+        <div className="feature-card  icon-book">
+          <span className="feature-icon">📚</span>
+          <h3>Rich Content</h3>
+          <p>Access comprehensive courses with video lectures and articles</p>
+        </div>
+        <div className="feature-card  icon-quiz">
+          <span className="feature-icon">✍️</span>
+          <h3>Interactive Quizzes</h3>
+          <p>Test your knowledge with engaging quizzes</p>
+        </div>
+        <div className="feature-card icon-progress">
+          <span className="feature-icon">📊</span>
+          <h3>Track Progress</h3>
+          <p>Monitor your learning journey with detailed progress tracking</p>
         </div>
       </div>
     </div>
